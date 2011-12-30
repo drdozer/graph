b7547787ee30cc39ddb1618e368632690ddff7d0
= <turingatemyhamster@gmail.com>
2011-12-30 16:57:26 +0000

    Scaladoc.

--- src/main/scala/uk/co/turingatemyhamster/graph/Triple.scala
+++ src/main/scala/uk/co/turingatemyhamster/graph/Triple.scala
@@ -5,10 +5,18 @@ import annotation.implicitNotFound
 /**
  * A triple is a labelled, directed edge linking a source and destination vertex.
  *
+ * @tparam V  vertex type
+ * @tparam E  edge type
+ *
+ * @see [[uk.co.turingatemyhamster.graph.Graph]]
+ *
  * @author Matthew Pocock
  */
 trait Triple[V, E] {
+  /** Source of the triple. */
   def source: V
+  /** Label of the triple. */
   def label: E
+  /** Destination of the triple. */
   def destination: V
 }
