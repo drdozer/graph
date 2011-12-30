b247d1a601827aaef928d5e1eaaa09905eb44a4b
Matthew Pocock <turingatemyhamster@gmail.com>
2011-12-30 13:30:36 +0000

    This compiles correctly now.

--- src/main/scala/uk/co/turingatemyhamster/graph/FiniteBinaryGraph.scala
+++ src/main/scala/uk/co/turingatemyhamster/graph/FiniteBinaryGraph.scala
@@ -1,17 +1,19 @@
 package uk.co.turingatemyhamster.graph
 
-class FiniteBinaryGraph[V, E](vertices: Set[V], incd: Map[E, (V, V)]) extends FiniteGraph[V, E] with BinaryGraph[V, E] {
+class FiniteBinaryGraph[V, E](verts: Set[V], incd: Map[E, (V, V)]) extends FiniteGraph[V, E] with BinaryGraph[V, E] {
   def edgesForVertex(v: V, i: Int) = i match {
     case 0 => edgesWhereVertexIsIncoming(v)
     case 1 => edgesWhereVertexIsOutgoing(v)
     case _ => throw new IndexOutOfBoundsException("Can't look up index %d in a pair".format(i))
   }
 
-  def edges = incd.keySet
+  def vertices: Col[V] = verts
+
+  def edges: Col[E] = incd.keySet
 
   def incidence(e: E) = incd(e)
 
-  def edgesWhereVertexIsIncoming(v: V) = incd.iterator.filter(_._1._1 == v).map(_._1)
+  def edgesWhereVertexIsIncoming(v: V): Col[E] = incd.filter(_._2._1 == v).map(_._1).toSet
 
-  def edgesWhereVertexIsOutgoing(v: V) = incd.iterator.filter(_._1._2 == v).map(_._1)
+  def edgesWhereVertexIsOutgoing(v: V): Col[E] = incd.filter(_._2._2 == v).map(_._1).toSet
 }
